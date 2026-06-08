fbeba9a516af868cdc7dfb17bba2a1228d17ae06
Nadibis <nadia041007@gmail.com>
2026-06-08 20:41:17 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,54 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() : countOp(0), first(nullptr) {}
+void Train::addCar(bool light) {
+  Car* newCar = new Car;
+  newCar->light = light;
+  newCar->next = nullptr;
+  newCar->prev = nullptr;
+
+  if (first == nullptr) {
+    first = newCar;
+    newCar->next = newCar;
+    newCar->prev = newCar;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
+}
+int Train::getLength() {
+  if (first == nullptr) return 0;
+  countOp = 0;
+  first->light = true;
+  int totalSteps = 0;      // общее число переходов
+  int memorizedLen = 0;    // сколько вагонов прошли не меняя направление
+  Car* current = first;
+  while (true) {
+    current = current->next;
+    countOp++;
+    totalSteps++;
+    memorizedLen++;
+    if (current->light == false) {
+      continue;
+    } else {
+      current->light = false;
+      for (int i = 0; i < memorizedLen; i++) { // назад на memorizedLen шагов
+        current = current->prev;
+        countOp++;
+      }
+    if (first->light == true) {
+        memorizedLen = 0;
+    } else {
+        return memorizedLen + 1;
+        break;
+      }
+    }
+  }
+}
+
+int Train::getOpCount() const {
+  return countOp;
+}
